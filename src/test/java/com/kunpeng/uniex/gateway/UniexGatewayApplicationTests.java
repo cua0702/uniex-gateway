4a57f4f896ea54e87bb4ca2357e6da15d49f88d3
cua0702 <503083597@qq.com>
2018-12-09 16:24:06 +0800

    初次提交

---
+++ src/test/java/com/kunpeng/uniex/gateway/UniexGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kunpeng.uniex.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UniexGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
